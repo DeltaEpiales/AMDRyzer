8c1f80c4af00da1c1c71cddcf0aec6155d018cfb
Ryan Osacra <32826618+DeltaEpiales@users.noreply.github.com>
2026-06-22 15:01:33 -0700

    Add CPU brand and cache information messages
    Extend stubs to current build - and end project with storage being on sim (theres wayy too much involved rn for byte to byte ntfs - i might come back to this)

--- AMDRyzer.asm
+++ AMDRyzer.asm
@@ -26,6 +26,14 @@ section .data
     msg_core_check   db 0xA, "Analyzing core configuration...", 0xA, 0
     msg_success      db "Operation completed successfully.", 0xA, 0xA, 0
     msg_vendor       db "Detected CPU Vendor: ", 0
+    msg_brand        db "Processor Brand: ", 0
+    msg_cache        db 0xA, "Cache Hierarchy:", 0xA, 0
+    msg_l1_dcache    db "  L1 Data Cache: ", 0
+    msg_l1_icache    db "  L1 Inst Cache: ", 0
+    msg_l2_cache     db "  L2 Cache: ", 0
+    msg_l3_cache     db "  L3 Cache: ", 0
+    msg_kb           db " KB", 0xA, 0
+    msg_privilege    db "  [Simulation Mode - Requires Ring-0 Kernel Access]", 0xA, 0
 
     ; Error messages
     err_avx          db "Error: AVX support not detected", 0xA, 0
@@ -40,9 +48,13 @@ section .data
 
     ; Format strings / Labels
     lbl_core         db "Core ", 0
-    lbl_temp         db " Temperature: 42°C (Simulated User-Space)", 0xA, 0
-    lbl_freq         db " Frequency: 3800 MHz (Simulated User-Space)", 0xA, 0
+    lbl_temp1        db " Temperature: ", 0
+    lbl_temp2        db " C", 0xA, 0
+    lbl_freq1        db " Frequency: ", 0
+    lbl_freq2        db " MHz", 0xA, 0
     lbl_newline      db 0xA, 0
+    path_thermal     db "/sys/class/thermal/thermal_zone0/temp", 0
+    path_freq        db "/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq", 0
 
     ; CPU feature detection masks
     avx2_mask        dq 0x00000020
@@ -190,6 +202,11 @@ storage_tools:
     mov rbp, rsp
     sub rsp, 128
 
+    mov rdi, msg_disk_check
+    call print_string
+    mov rdi, msg_privilege
+    call print_string
+
     call init_disk_subsystem
     
     mov rdi, disk_buffer
@@ -214,6 +231,8 @@ boot_repair:
 
     mov rdi, msg_boot_check
     call print_string
+    mov rdi, msg_privilege
+    call print_string
 
     call analyze_boot_sector
     call check_partition_table
@@ -231,6 +250,8 @@ performance_optimizer:
     sub rsp, 64
 
     mov rdi, msg_perf_check
+    call print_string
+    mov rdi, msg_privilege
     call print_string
 
     call analyze_performance_state
@@ -269,7 +290,14 @@ temperature_monitor:
     call print_string
     mov rdi, rbx
     call print_number
-    mov rdi, lbl_temp
+    
+    mov rdi, lbl_temp1
+    call print_string
+    
+    mov rdi, [core_temps + rbx*8]
+    call print_number
+    
+    mov rdi, lbl_temp2
     call print_string
     
     inc rbx
@@ -418,10 +446,158 @@ check_cpu_vendor:
     ret
 
 enable_cpu_features:      ret
-get_cpu_details:          mov rax, 1 \ ret
+get_cpu_details:
+    push rbx
+    push rcx
+    push rdx
+    
+    ; Check if extended CPUID is supported
+    mov eax, 0x80000000
+    cpuid
+    cmp eax, 0x80000004
+    jb .done
+    
+    mov rdi, cpu_info
+    
+    ; Get CPU brand string part 1
+    mov eax, 0x80000002
+    cpuid
+    mov [rdi], eax
+    mov [rdi+4], ebx
+    mov [rdi+8], ecx
+    mov [rdi+12], edx
+    
+    ; Get CPU brand string part 2
+    mov eax, 0x80000003
+    cpuid
+    mov [rdi+16], eax
+    mov [rdi+20], ebx
+    mov [rdi+24], ecx
+    mov [rdi+28], edx
+    
+    ; Get CPU brand string part 3
+    mov eax, 0x80000004
+    cpuid
+    mov [rdi+32], eax
+    mov [rdi+36], ebx
+    mov [rdi+40], ecx
+    mov [rdi+44], edx
+    
+    mov byte [rdi+48], 0
+    
+    ; Print it
+    mov rdi, msg_brand
+    call print_string
+    mov rdi, cpu_info
+    call print_string
+    mov rdi, lbl_newline
+    call print_string
+    
+.done:
+    mov rax, 1
+    pop rdx
+    pop rcx
+    pop rbx
+    ret
+
 analyze_cpu_topology:     ret
-test_cache_hierarchy:     ret
-measure_frequencies:      ret
+
+test_cache_hierarchy:
+    push rbx
+    push rcx
+    push rdx
+    
+    ; Check if cache topology CPUID is supported
+    mov eax, 0x80000000
+    cpuid
+    cmp eax, 0x80000006
+    jb .done
+    
+    mov rdi, msg_cache
+    call print_string
+    
+    ; Get L1 Cache Info (AMD uses 0x80000005)
+    mov eax, 0x80000005
+    cpuid
+    
+    ; L1 Data Cache (ecx contains size in KB in high 8 bits)
+    mov r8d, ecx
+    shr r8d, 24
+    
+    ; L1 Instruction Cache (edx contains size in KB in high 8 bits)
+    mov r9d, edx
+    shr r9d, 24
+    
+    ; Print L1 D-Cache
+    mov rdi, msg_l1_dcache
+    call print_string
+    mov rdi, r8
+    call print_number
+    mov rdi, msg_kb
+    call print_string
+    
+    ; Print L1 I-Cache
+    mov rdi, msg_l1_icache
+    call print_string
+    mov rdi, r9
+    call print_number
+    mov rdi, msg_kb
+    call print_string
+    
+    ; Get L2/L3 Cache Info
+    mov eax, 0x80000006
+    cpuid
+    
+    ; L2 Cache size is in ECX [31:16] in KB
+    mov r8d, ecx
+    shr r8d, 16
+    
+    ; L3 Cache size is in EDX [31:18] in 512KB chunks
+    mov r9d, edx
+    shr r9d, 18
+    shl r9d, 9  ; Multiply by 512
+    
+    ; Print L2 Cache
+    mov rdi, msg_l2_cache
+    call print_string
+    mov rdi, r8
+    call print_number
+    mov rdi, msg_kb
+    call print_string
+    
+    ; Print L3 Cache
+    mov rdi, msg_l3_cache
+    call print_string
+    mov rdi, r9
+    call print_number
+    mov rdi, msg_kb
+    call print_string
+
+.done:
+    pop rdx
+    pop rcx
+    pop rbx
+    ret
+measure_frequencies:
+    push rbp
+    mov rbp, rsp
+    
+    call read_core_freq
+    test rax, rax
+    jz .done
+    
+    push rax
+    mov rdi, lbl_freq1
+    call print_string
+    pop rdi
+    call print_number
+    mov rdi, lbl_freq2
+    call print_string
+    
+.done:
+    mov rsp, rbp
+    pop rbp
+    ret
 display_cpu_report:       mov rdi, msg_success \ call print_string \ ret
 analyze_memory_channels:  ret
 check_memory_timings:     ret
@@ -442,7 +618,86 @@ optimize_core_config:     ret
 apply_performance_settings: ret
 verify_optimization:      mov rdi, msg_success \ call print_string \ ret
 init_temp_monitor:        ret
-read_core_temp:           mov rax, 42 \ ret
+
+; System read helper
+read_int_from_file:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 64
+    
+    ; open
+    mov rax, 2      ; sys_open
+    xor rsi, rsi    ; O_RDONLY
+    xor rdx, rdx
+    syscall
+    test rax, rax
+    js .error
+    
+    mov r8, rax     ; save fd
+    
+    ; read
+    mov rdi, rax    ; fd
+    mov rax, 0      ; sys_read
+    lea rsi, [rsp]  ; buffer on stack
+    mov rdx, 63     ; max bytes
+    syscall
+    test rax, rax
+    jle .close_error
+    
+    ; null-terminate
+    mov byte [rsp+rax], 0
+    
+    ; parse integer
+    lea rsi, [rsp]
+    xor rax, rax
+    xor rcx, rcx
+.parse_loop:
+    movzx rdx, byte [rsi+rcx]
+    cmp dl, '0'
+    jl .done_parse
+    cmp dl, '9'
+    jg .done_parse
+    sub dl, '0'
+    imul rax, 10
+    add rax, rdx
+    inc rcx
+    jmp .parse_loop
+    
+.done_parse:
+    mov r9, rax     ; save result
+    
+    ; close
+    mov rax, 3      ; sys_close
+    mov rdi, r8     ; fd
+    syscall
+    
+    mov rax, r9     ; restore result
+    jmp .exit
+    
+.close_error:
+    mov rax, 3
+    mov rdi, r8
+    syscall
+.error:
+    xor rax, rax
+.exit:
+    mov rsp, rbp
+    pop rbp
+    ret
+
+read_core_temp:
+    push rbp
+    mov rbp, rsp
+    mov rdi, path_thermal
+    call read_int_from_file
+    ; thermal_zone temp is in millidegrees, divide by 1000
+    mov rcx, 1000
+    xor rdx, rdx
+    div rcx
+    mov rsp, rbp
+    pop rbp
+    ret
+
 check_thermal_throttling: ret
 display_thermal_report:   mov rdi, msg_success \ call print_string \ ret
 get_core_config:          ret
@@ -454,8 +709,21 @@ clear_buffer:             ret
 read_sector:              mov rax, 1 \ ret
 verify_sector_checksum:   mov rax, 1 \ ret
 setup_perf_counter:       ret
-read_core_freq:           mov rax, 3800 \ ret
-read_core_voltage:        mov rax, 1200 \ ret
+
+read_core_freq:
+    push rbp
+    mov rbp, rsp
+    mov rdi, path_freq
+    call read_int_from_file
+    ; cpufreq scaling_cur_freq is in KHz, divide by 1000 to get MHz
+    mov rcx, 1000
+    xor rdx, rdx
+    div rcx
+    mov rsp, rbp
+    pop rbp
+    ret
+
+read_core_voltage:        mov rax, 0 \ ret
 log_error:                ret
 print_error_message:      ret
 cleanup_system:           ret
